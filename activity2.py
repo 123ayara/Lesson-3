d34f5d9f1b6bf63cbb4a611872490c120e3f0560
Ayara <sushma.paudel4317@gmail.com>
2025-01-28 17:44:23 -0700

    first commit

---
+++ activity2.py
@@ -0,0 +1,12 @@
+name=("penguin")
+age=15
+is_student=True
+weight=38.5
+print("Name:",name)
+print("data type of name is:",type(name))
+print("Age:",age)
+print("data type of age is:",type(age))
+print("Is student:",is_student)
+print("data type of is a student is:",type(is_student))
+print("Weight:",weight)
+print("data type of weight is:",type(weight))
