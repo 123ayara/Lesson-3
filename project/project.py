501ca9ea03a1ed7c7caf2713933b5602f54ec952
Ayara <sushma.paudel4317@gmail.com>
2025-02-04 17:20:29 -0700

    First Publish

---
+++ project/project.py
@@ -0,0 +1,11 @@
+print("in order of oldest to youngest:")
+a="Ayara:6/1/2013"
+c="Coleen:7/10/2013"
+m="Mora:4/26/2013"
+l="Logan:5/31/2013"
+x="Xavier:3/11/2013"
+print(x)
+print(m)
+print(l)
+print(a)
+print(c)
